45d68224c8d197df55e20967bb2c44dcbe8b1f4b
umasuo <liuquan89@gmail.com>
2017-02-13 14:25:15 +0800

    init customer-info service

---
+++ src/main/java/io/reactivesw/customer/info/Application.java
@@ -0,0 +1,19 @@
+package io.reactivesw.customer.info;
+
+import org.apache.log4j.Logger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by umasuo on 17/2/9.
+ */
+@SpringBootApplication
+public class Application {
+
+  private static final Logger LOGGER = Logger.getLogger(Application.class);
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+}
