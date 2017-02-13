cf49baceb0ca4e5332d8604721aab9434a2e902a
umasuo <liuquan89@gmail.com>
2017-02-13 17:27:04 +0800

    add get by customer id api

--- src/main/java/io/reactivesw/customer/info/application/controller/CustomerController.java
+++ src/main/java/io/reactivesw/customer/info/application/controller/CustomerController.java
@@ -13,6 +13,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -42,11 +43,6 @@ public class CustomerController {
    */
   private static final String EXIT_LOG_INFO = "exit: customer: {}";
 
-  /**
-   * customer repository.
-   */
-  @Autowired
-  private transient CustomerRepository customerRepository;
 
   /**
    * get customer by id.
@@ -54,27 +50,14 @@ public class CustomerController {
    * @param id String
    * @return CustomerEntity
    */
-  public Customer getById(String id) {
+  @GetMapping(Router.CUSTOMER_ID)
+  public CustomerView getById(@PathVariable String id) {
     LOG.debug("enter: id:{}", id);
-    Customer entity = this.customerRepository.findOne(id);
-    if (entity == null) {
-      LOG.warn("customer not exist: id:{}", id);
-      throw new NotExistException("customer not exist. id:" + id);
-    }
-    LOG.debug("exit: id:{}, customer:{}", id, entity);
-    return entity;
-  }
 
-  /**
-   * create customer with sample.
-   * used for sign up.
-   *
-   * @param customer CustomerEntity
-   * @return CustomerEntity
-   */
-  public Customer create(Customer customer) {
-    LOG.debug("enter: sample: {}", customer);
-    return this.customerRepository.save(customer);
+    Customer customer = customerService.getById(id);
+
+    LOG.debug("exit: id:{}, customer:{}", id, customer);
+    return CustomerMapper.entityToModel(customer);
   }
 
   /**
