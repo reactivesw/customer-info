bc75c76e5845836cb5503c152482ea0f624cdfaf
umasuo <liuquan89@gmail.com>
2017-02-22 15:44:40 +0800

    add test controller

--- src/main/java/io/reactivesw/customer/info/Application.java
+++ src/main/java/io/reactivesw/customer/info/Application.java
@@ -8,7 +8,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * Created by umasuo on 17/2/9.
  */
 @EnableSwagger2
-@SpringBootApplication(scanBasePackages = "com.reactivesw")
+@SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
---
+++ src/main/java/io/reactivesw/customer/info/application/controller/TestController.java
@@ -0,0 +1,16 @@
+package io.reactivesw.customer.info.application.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by umasuo on 17/2/22.
+ */
+@RestController
+public class TestController {
+
+  @GetMapping("/test")
+  public String testName() {
+    return "customerInfo: " + System.currentTimeMillis();
+  }
+}
