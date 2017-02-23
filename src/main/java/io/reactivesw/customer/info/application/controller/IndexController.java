7e35c503e41c6cb6cb69ca2ea061ae3f0c4600f0
umasuo <liuquan89@gmail.com>
2017-02-23 19:45:00 +0800

    add health check api

--- src/main/java/io/reactivesw/customer/info/application/controller/IndexController.java
+++ src/main/java/io/reactivesw/customer/info/application/controller/IndexController.java
@@ -1,26 +1,30 @@
 package io.reactivesw.customer.info.application.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Created by umasuo on 17/2/22.
+ * Created by umasuo on 17/2/21.
  */
 @RestController
+@Configuration
 public class IndexController {
 
-  @GetMapping("/test/abc")
-  public String testSub() {
-    return "customerInfo test sub: " + System.currentTimeMillis();
-  }
+  /**
+   * service name.
+   */
+  @Value("${spring.controller.name}")
+  private String serviceName;
 
-  @GetMapping("/test")
-  public String testRoot() {
-    return "customerInfo: " + System.currentTimeMillis();
-  }
-
+  /**
+   * this api is used for health check.
+   *
+   * @return service name.
+   */
   @GetMapping("/")
   public String index() {
-    return "Customer-info service, system time: " + System.currentTimeMillis();
+    return serviceName + ", system time: " + System.currentTimeMillis();
   }
 }
