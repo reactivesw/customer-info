dfaec7df3d81beef7debd3bfaa5080796d55df93
Davis <davis.dai@go6d.com>
2017-04-17 12:00:49 +0800

    edit deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -47,7 +47,7 @@ echo '=============================finish gcloud container cluster credentials==
 
 #kubectl create deployments and service by .yaml file
 echo '=============================kubectl create yaml==================================='
-kubectl delete deployment customer-info --ignore-not-found=true
-kubectl delete service customer-info --ignore-not-found=true
+kubectl delete deployment customer-info --namespace=dev --ignore-not-found=true
+kubectl delete service customer-info --namespace=dev --ignore-not-found=true
 kubectl create -f ./deploy/k8s.yaml
 echo '=============================finish kubectl create yaml============================'
